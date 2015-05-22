4651b27e9229ebeff065db8ecaf98e0bfdbc7c29
Khomac <khomac666@gmail.com>
2015-05-22 22:53:33 +0200

    Poczatek
    problemy z przesuwaniem okienek podczas tworzenia

---
+++ src/planningfor24hprojects/PlanningFor24hProjects.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package planningfor24hprojects;
+
+/**
+ *
+ * @author Komac
+ */
+public class PlanningFor24hProjects
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        BaseWindow planning = new BaseWindow();
+        planning.setVisible(true);
+    }
+    
+}
